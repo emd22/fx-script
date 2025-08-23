b40d4df2e9840008051cc92dd2a80b7652409211
Ethan MacDonald <e@ethanm.ca>
2025-08-23 15:30:40 -0300

    Added clang config files, added ifdef guard for pause in main

--- Main.cpp
+++ Main.cpp
@@ -1,52 +1,47 @@
-#include <cstdio>
-
-#include <string>
-#include <assert.h>
-
 #include "FxScript.hpp"
 
+#include <assert.h>
+
+#include <cstdio>
+#include <string>
 
 int main()
 {
-	FxConfigScript script;
-	script.LoadFile("Main.fxS");
+    FxConfigScript script;
+    script.LoadFile("Main.fxS");
 
-	script.DefineExternalVar("playerid", "emd22", FxScriptValue(FxScriptValue::INT, 1020));
+    script.DefineExternalVar("playerid", "emd22", FxScriptValue(FxScriptValue::INT, 1020));
 
-	FxScriptVM vm ;
+    FxScriptVM vm;
 
-	script.Execute(vm);
+    script.Execute(vm);
 
-	std::string command = "";
+    std::string command = "";
 
-	printf(
-		"\nFoxtrot Script\nVersion %d.%d.%d\n",
-		FX_SCRIPT_VERSION_MAJOR,
-		FX_SCRIPT_VERSION_MINOR,
-		FX_SCRIPT_VERSION_PATCH
-	);
+    printf("\nFoxtrot Script\nVersion %d.%d.%d\n", FX_SCRIPT_VERSION_MAJOR, FX_SCRIPT_VERSION_MINOR, FX_SCRIPT_VERSION_PATCH);
 
+    // while (true) {
+    // 	printf(" -> ");
+    // 	fflush(stdout);
 
-	// while (true) {
-	// 	printf(" -> ");
-	// 	fflush(stdout);
+    // 	std::getline(std::cin, command);
 
-	// 	std::getline(std::cin, command);
+    // 	if (command == "quit") {
+    // 		break;
+    // 	}
 
-	// 	if (command == "quit") {
-	// 		break;
-	// 	}
+    // 	if (!command.ends_with(';')) {
+    // 		command += ';';
+    // 	}
 
-	// 	if (!command.ends_with(';')) {
-	// 		command += ';';
-	// 	}
+    // 	//printf("Executing command {%s}\n", command.c_str());
 
-	// 	//printf("Executing command {%s}\n", command.c_str());
+    // 	script.ExecuteUserCommand(command.c_str(), interpreter);
+    // }
 
-	// 	script.ExecuteUserCommand(command.c_str(), interpreter);
-	// }
+#ifdef _WIN32
+    system("pause");
+#endif
 
-	system("pause");
-
-	return 0;
+    return 0;
 }
